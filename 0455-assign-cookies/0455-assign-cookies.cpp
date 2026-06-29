e459a241a061d468b401ace392efd2601b0281f4
DeepTandel045 <tandeldeep6106@gmail.com>
2026-06-29 23:08:25 +0530

    Time: 8 ms (58.74%), Space: 44.9 MB (47.77%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+
+        int n = g.size(), m = s.size();
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int l = 0;
+        int r = 0;
+
+        while (r < m && l < n) {
+            if (g[l] <= s[r]) {
+
+                l++;
+            }
+            r++;
+        }
+
+        return l;
+    }
+};
