6d0415401f784efa178c77cd64406fffec9c46d0
DeepTandel045 <tandeldeep6106@gmail.com>
2026-07-15 17:31:55 +0530

    Time: 0 ms (100%), Space: 8.3 MB (21.98%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+
+        vector<int>a;
+
+        for(int i =1;i*i<=n;i++) {
+            if(n%i == 0) {
+                a.push_back(i);
+            
+
+            if ( i != n/i) {
+                a.push_back(n/i);
+            } 
+            }
+        }
+
+        sort(a.begin(),a.end());
+
+
+        if(k <= a.size()) {
+            return a[k-1];
+        }
+
+
+        return -1;
+        
+    }
+};
