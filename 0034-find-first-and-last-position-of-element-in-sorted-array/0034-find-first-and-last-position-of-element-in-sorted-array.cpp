2a50cc59529d0e454d7d7599420989564dacb4ca
DeepTandel045 <tandeldeep6106@gmail.com>
2026-08-03 21:12:59 +0530

    Time: 0 ms (100%), Space: 17.7 MB (20.33%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,37 +1,50 @@
 class Solution {
 public:
+
+ int FO(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return ans;
+    }
+
+    int LO(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return ans;
+    }
+
+    
     vector<int> searchRange(vector<int>& nums, int target) {
 
-        int n = nums.size();
-        
-        int c1 = 0,c2=0;
-        
-        vector<int>a;
-        
-        for(int i =0;i<n;i++) {
-            if(nums[i] == target) {
-                a.push_back(i);
-                c1++;
-                break;
-            }
-        }
-        
-        for(int i = n-1;i>=0;i--) {
-            if(nums[i] == target) {
-                a.push_back(i);
-                c2++;
-                break;
-            }
-        }
-        
-        if(c1 == 0 && c2 == 0) {
-            a.push_back(-1);
-            a.push_back(-1);
-            
-            
-        }
-        
-        return a;
+       return {FO(nums,target),LO(nums,target)};
         
     }
 };
