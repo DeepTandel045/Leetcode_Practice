c0c5d325cf7f42c49928efc996844efe75cd2e79
DeepTandel045 <tandeldeep6106@gmail.com>
2026-09-03 21:22:23 +0530

    Time: 4 ms (34.18%), Space: 18.2 MB (28.37%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -18,9 +18,10 @@ public:
             v.push_back({i.first,i.second});
         }
 
-        sort(v.begin(),v.end(),[](pair<int,int>&a,pair<int,int>&b) {
+        
+        sort(v.begin(),v.end(),[](auto &a,auto &b){
             return a.second>b.second;
-                    });
+        });
 
         for(int i=0;i<k;i++) {
             a.push_back(v[i].first);
