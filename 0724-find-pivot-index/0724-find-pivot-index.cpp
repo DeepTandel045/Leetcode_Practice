219142b11c499e83816f7e3cb298a221b5cbfa18
DeepTandel045 <tandeldeep6106@gmail.com>
2026-09-09 21:59:21 +0530

    Time: 747 ms (5.03%), Space: 35.6 MB (79.16%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        int k =0;
+        
+        while(k<n) {
+
+            long long lsum=0,rsum=0;
+
+        for(int i =0;i<k;i++) {
+            lsum += nums[i];
+        }
+
+        for(int i = k+1;i<n;i++) {
+            rsum += nums[i];
+        } 
+        
+        k++;
+
+
+        if(lsum == rsum ) return k-1;
+
+        }
+        
+        return -1;
+    }
+};
